0cbf878112457ae0329e1271b6e9eb07225340ab
Sathyagyajangi481 <sathyagyajangi481@gmail.com>
2019-12-29 23:38:31 +0530

    first

--- FindingBrokenImages/FunctionLibrary.cs
+++ FindingBrokenImages/FunctionLibrary.cs
@@ -88,8 +88,7 @@ namespace FindingBrokenImages
         {
             excel.Application xlapp = new excel.Application();
 
-            excel.Workbook xlworkbook = xlapp.Workbooks.Open(@"C:\Users\Satyanarayan\source\git\BrokenImages\FindingBrokenImages\TestInput\input.xlsx");
-
+            excel.Workbook xlworkbook = xlapp.Workbooks.Open(@"C:\\Users\\shaik\\source\\git\\FindBrokenImages\\FindingBrokenImages\\TestInput\\input.xlsx"); 
             excel._Worksheet xlworksheet = xlworkbook.Sheets[S];
 
             excel.Range xlrange = xlworksheet.UsedRange;
